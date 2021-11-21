c619eb49baff28338066d8acbf5b8cfe4d60afb8
Javier-I-Morales <javierignamorales@gmail.com>
2021-11-21 12:41:20 -0300

    varios arreglos, falta algo

--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -1,23 +1,30 @@
 
 
-const Ciudad = ({lugar, setCiudad, setProvincia, setTemperatura, setHumedad, setVisibilidad, setPresion, setDescripcion}) => {
 
-    function mostrarDato1(lugar){
+const Ciudad = ({clima, nombre, setCiudad, setProvincia, setTemperatura, setHumedad, setVisibilidad, setPresion, setDescripcion}) => {
 
-        setCiudad(lugar.name)
-        setProvincia(lugar.province)
+    
 
-        setTemperatura(lugar.weather.tempDesc)
-        setHumedad(lugar.weather.humidity)
-        setVisibilidad(lugar.weather.visibility)
-        setPresion(lugar.weather.pressure)
-        setDescripcion(lugar.weather.description)
+    function mostrarDato(){
+
+        let lugar = []
+
+        lugar = clima.filter( c => c.name === nombre)
+
+        setCiudad(lugar[0].name)
+        setProvincia(lugar[0].province)
+        
+        setTemperatura(lugar[0].weather.tempDesc)
+        setHumedad(lugar[0].weather.humidity)
+        setVisibilidad(lugar[0].weather.visibility)
+        setPresion(lugar[0].weather.pressure)
+        setDescripcion(lugar[0].weather.description)
         console.log(lugar)
     }
 
     return ( 
         <div className="list-group-item ">
-            <h5  className=" ciudad_enlace" onClick={()=>{mostrarDato1(lugar)}}>{lugar.name}</h5>
+            <h5  className=" ciudad_enlace" onClick={()=>{mostrarDato()}}>{nombre}</h5>
         </div>
      );
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,19 +7,33 @@ const Container = () => {
 
     const [clima, setClima] = useState([])
 
+    const climaOrdenado = []
+ 
     const [ciudad, setCiudad] = useState()
     const [provincia, setProvincia] = useState()
 
     const [temperatura, setTemperatura] = useState()
-    const [hummedad, setHumedad] = useState()
+    const [humedad, setHumedad] = useState()
     const [visibilidad, setVisibilidad] = useState()
     const [presion, setPresion] = useState()
     const [descripcion, setDescripcion] = useState()
 
     useEffect( () => {
         traeClima()
+        
     },[])
  
+    ordenaClima();
+
+    function ordenaClima(){
+
+        for(let ciudad of clima){
+            climaOrdenado.push(ciudad.name)
+        }
+        climaOrdenado.sort() 
+    }
+
+
     const traeClima = async () => {
         try {
             const api = await fetch('https://ws.smn.gob.ar/map_items/weather');
@@ -31,8 +45,44 @@ const Container = () => {
         }
     }
 
-    return ( 
+    function mostrarDato(lugar){
+
+        setCiudad(lugar.name)
+        setProvincia(lugar.province)
+
+        setTemperatura(lugar.weather.tempDesc)
+        setHumedad(lugar.weather.humidity)
+        setVisibilidad(lugar.weather.visibility)
+        setPresion(lugar.weather.pressure)
+        setDescripcion(lugar.weather.description)
+        console.log(lugar)
+    }
+
+    const formulario = document.querySelector('#formulario');
+
+    function cleanCadena(texto) {
+        return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+       }
+    
+    const filtrar = () =>{
+
+        const texto = cleanCadena(formulario.value.toLowerCase());
+
+        console.log(texto);
         
+        for(let ciudad of clima){
+            let nombre = cleanCadena(ciudad.name.toLowerCase());
+            if(nombre === texto){
+                console.log(nombre);
+                mostrarDato(ciudad); 
+            }
+        }
+    }
+
+    return ( 
+
+        
+
         <div className="container">
             <div className="row altura_total_datos">
 
@@ -40,13 +90,22 @@ const Container = () => {
                 <div><br /></div>
                 
                 <div className="col-sm-12 col-md-6 col-lg-4">
-                    <div className="lista">
+                    <div >
                         <div className="container" >
-                            {clima.map( item => 
+                        <label  className="form-label"><h4 className="subtitulos">Buscador</h4></label>
+                        
+                        <input className="form-control" type='text' id='formulario' placeholder="Escriba su ciudad aquí..."/><button className='btn btn-info' id='boton' onClick={filtrar}>Buscar</button>
+             
+                        <br /><br />
+                        <div className="lista">
+                            <div className="container" >
+                            {climaOrdenado.map( (item, index) => 
+                            
                             (
                                 <Ciudad 
-                                    key = {item._id}
-                                    lugar = {item}
+                                    key = {index}
+                                    clima ={clima}
+                                    nombre = {item}
                                     setCiudad ={setCiudad}
                                     setProvincia = {setProvincia}
                                     setTemperatura = {setTemperatura}
@@ -57,65 +116,99 @@ const Container = () => {
                                 />
                             )
                             )}
+                                
+                            </div>
                         </div>
+                            
+                        </div>
+                        
                     </div>
+                        
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-8">
                     <div className="row container">
-                    <div className="subtitulo_datos"><h3>Datos:</h3></div>
-                        <div className="datos">
+                    <div className="subtitulos"><h4>Datos</h4></div>
+                        <div className='marco'>
                             <h2 style={{
                                 textAlign: 'center', 
                             }}>{ciudad}</h2>
-                            <div className="d-flex justify-content-center color_Provincia"><h4>Provincia: {provincia}</h4></div>
+                            
+                            <div className="d-flex justify-content-center "><h4 style={{
+                                textAlign: 'center', 
+                            }}>{provincia}.</h4></div>
                             
                         </div>
                     </div><br />
                     <div className="row container">
-                        <div className="datos   ">
-                            <div className="row">
-                                <div className="col" style={{
-                                textAlign: 'center', 
-                            }}>
-                                    <h6>Temperatura: </h6><h3>{temperatura}</h3>
-                                </div>
-                                <div className="col" style={{
-                                textAlign: 'center', 
-                            }}>
-                                    <h6>Humedad: </h6><h3>{hummedad} %</h3>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col" style={{
-                                textAlign: 'center', 
-                            }}>
-                                    <h6>Visibilidad: </h6><h3>{visibilidad} KM</h3>
-                                </div>
-                                <div className="col" style={{
-                                textAlign: 'center', 
-                            }}>
-                                    <h6>Presion: </h6><h3>{presion} hPa</h3>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div><br />
-                    <div className="row container">
-                    <div className="datos">
+                    <div className="marco">
                             <div style={{
                                 textAlign: 'center', marginTop: '10px', 
                             }}><h6>Descripción:</h6></div>
-                            <div className="d-flex justify-content-center color_Provincia"><h3> {descripcion}.</h3></div>
+                            <div className="d-flex justify-content-center"><h3> {descripcion}.</h3></div>
                             
                         </div>
                     
                     </div>
-                </div>
+                    <br />
+                    <div className=" row container">
+                        <div className="col-sm-12 col-md-6 col-lg-6" style={{ paddingBottom:10}}>
+                            
+                                <div className="marco" style={{
+                                textAlign: 'center',
+                                paddingTop:25,
+                                paddingBottom:25, 
+                            }}>
+                                    <h1>holaa</h1>
+                                </div>
+                            
+                        </div >
+                        
+                        <div className="col-sm-12 col-md-6 col-lg-6">
+                            <div className="marco" style={{
+                                    textAlign: 'center',
+                                    paddingTop:25,
+                                    paddingBottom:25, 
+                                }}>
 
+                                <div className="row">
+                                    <div className="col" style={{
+                                    textAlign: 'center',
+                                    paddingTop:25,
+                                    paddingBottom:25, 
+                                }}>
+                                        <h6>Temperatura: </h6><h3>{temperatura}</h3>
+                                    </div>
+                                    <div className="col" style={{
+                                    textAlign: 'center', 
+                                    paddingTop:25,
+                                    paddingBottom:25,
+                                }}>
+                                        <h6>Humedad: </h6><h3>{humedad} %</h3>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col" style={{
+                                    textAlign: 'center', 
+                                    paddingTop:25,
+                                    paddingBottom:25,
+                                }}>
+                                        <h6>Visibilidad: </h6><h3>{visibilidad} KM</h3>
+                                    </div>
+                                    <div className="col" style={{
+                                    textAlign: 'center', 
+                                    paddingTop:25,
+                                    paddingBottom:25,
+                                }}>
+                                        <h6>Presion: </h6><h3>{presion} hPa</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                </div>
             </div>
-            
         </div>
-        
         );
 }
  
